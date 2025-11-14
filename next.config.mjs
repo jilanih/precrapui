3fb0ec4abfbb5d1ad2ad8dd1f1ed0e00972faf90
Jilani <jilanih@amazon.com>
2025-11-14 14:36:43 -0800

    Add bucket and region env vars to next.config

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,8 @@ const nextConfig = {
   env: {
     S3_ACCESS_KEY_ID: process.env.S3_ACCESS_KEY_ID,
     S3_SECRET_ACCESS_KEY: process.env.S3_SECRET_ACCESS_KEY,
+    NEXT_PUBLIC_AWS_REGION: process.env.NEXT_PUBLIC_AWS_REGION,
+    NEXT_PUBLIC_S3_BUCKET: process.env.NEXT_PUBLIC_S3_BUCKET,
   },
 }
 
